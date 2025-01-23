da40a29719f765bb5407f3ec895e525f9498a18b
harsh <harshvaish2003@gmail.com>
2025-01-23 20:24:26 +0530

    modified nextconfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/a/**",
-      },
-    ],
-  },
+    output: 'standalone',
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/a/**",
+            },
+        ],
+    },
+    env: {
+        NODE_ENV: process.env.NODE_ENV,
+        DATABASE_URL: process.env.DATABASE_URL,
+        DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
+        YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
+        HOST_NAME: process.env.HOST_NAME,
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+        SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL,
+        BASE_URL: process.env.BASE_URL,
+    }
 };
 
 export default nextConfig;
